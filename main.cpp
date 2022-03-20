60d2322d9870d59e6eca9e1d38d0bb9b97cb85ab
Tony R <46798310+TonyR43@users.noreply.github.com>
2022-03-19 20:17:01 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <wingdi.h>
+
+
+int main() {
+    int N, i, j;
+
+    FILE* f;
+
+    f = fopen("file.txt", "w");
+
+    cin >> n;
+
+    vector<int> name(N);
+
+}
