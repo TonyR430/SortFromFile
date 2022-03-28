6d165702a4ce3bc7386e386fd9fc4fea26e114dd
Tony R <46798310+TonyR43@users.noreply.github.com>
2022-03-28 07:36:54 -0500

    Finish base code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <fstream>
 #include <wingdi.h>
+using namespace std;
 
 
 int main() {
@@ -17,4 +18,37 @@ int main() {
 
     vector<int> name(N);
 
-}
+    for (i=0; i < n; i++)
+    {
+        cin >> name [i];
+
+        fprintf(f, "%s", name[i]);
+    }
+
+    fclose(f);
+
+    f= fopen("file.txt", "r");
+
+    if (f==NULL)
+    {
+        cout << "File doesn't exist!";
+        return 0;
+    }
+
+    while (!feof(f)) {
+        fscanf(f, "%s", name[i]);
+        i++;
+    }
+    n=i-1;
+
+    sort(name.begin(), name.end());
+    for (i = 0; i < n; i++) {
+        fprintf(f, "%s", name[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        cout << name[i] << '\n';
+    }
+    return 0;
+    }
+
